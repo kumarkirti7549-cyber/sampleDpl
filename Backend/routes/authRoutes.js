3f8d619dddff94dfbf9706a8a28273ff785fd478
kumarkirti7549-cyber <kumarkirti7549@gmail.com>
2026-07-14 12:12:42 +0530

    Fix controller filename

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {
   registerUser,
   loginUser,
-} = require("../controllers/authController");
+} = require("../controllers/authcontroller");
 
 router.post("/register", registerUser);
 
